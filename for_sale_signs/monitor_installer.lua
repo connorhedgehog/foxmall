a33973e0f135c4d672d221c109a9e66339e49d22
connor <70295743+connorhedgehog@users.noreply.github.com>
2025-07-19 16:15:29 -0500

    Fixed the monitor size check

--- for_sale_signs/monitor_installer.lua
+++ for_sale_signs/monitor_installer.lua
@@ -1,7 +1,7 @@
 local monitor = peripheral.find("monitor")
 local monitor_x,_ = monitor.getSize()
 
-if monitor_x == 5 then
+if monitor_x == 50 then
     shell.run("wget https://raw.githubusercontent.com/connorhedgehog/foxmall/refs/heads/main/for_sale_signs/monitor_5x4.lua startup.lua")
 else
     shell.run("wget https://raw.githubusercontent.com/connorhedgehog/foxmall/refs/heads/main/for_sale_signs/monitor_6x4.lua startup.lua")
